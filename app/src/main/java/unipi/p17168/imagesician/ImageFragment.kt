32b5ae381e9751f40b0eaecf7b893ab6c7be374d
zartilas <36131492+zartilas@users.noreply.github.com>
2021-05-12 18:22:18 +0300

    Bugs Fixes and Imported files

--- app/src/main/java/unipi/p17168/imagesician/ImageFragment.kt
+++ app/src/main/java/unipi/p17168/imagesician/ImageFragment.kt
@@ -1,21 +1,34 @@
 package unipi.p17168.imagesician
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import androidx.fragment.app.Fragment
 
 class ImageFragment : Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_image, container, false)
+    private lateinit var binding : ImageFragment
+    private var imageText = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+
     }
 
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+
+
+        return inflater.inflate(R.layout.fragment_image, container, false)
+
+    }
+
+
     fun detection() {
-       // val image = FirebaseVisionImage.fromBitmap(bitmap)
+
     }
 
 }
