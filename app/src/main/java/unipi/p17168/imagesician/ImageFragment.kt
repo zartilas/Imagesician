2e616f07bedaa195a9127caa04ea6f1578d1e848
zartilas <36131492+zartilas@users.noreply.github.com>
2021-05-23 19:20:12 +0300

    Bugs Fixes and imported rotation Image

--- app/src/main/java/unipi/p17168/imagesician/ImageFragment.kt
+++ app/src/main/java/unipi/p17168/imagesician/ImageFragment.kt
@@ -2,13 +2,19 @@ package unipi.p17168.imagesician
 
 import android.annotation.SuppressLint
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
 import android.graphics.Bitmap
+import android.graphics.Matrix
+import android.media.ExifInterface
+import android.net.Uri
+import android.net.Uri.parse
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import com.google.android.material.chip.Chip
 import com.google.mlkit.vision.common.InputImage
@@ -16,9 +22,11 @@ import com.google.mlkit.vision.label.ImageLabeling
 import com.google.mlkit.vision.label.defaults.ImageLabelerOptions
 import com.google.mlkit.vision.objects.ObjectDetection
 import com.google.mlkit.vision.objects.defaults.ObjectDetectorOptions
-
+import com.google.mlkit.vision.text.TextRecognition
 import unipi.p17168.imagesician.databinding.FragmentImageBinding
+import java.io.ByteArrayOutputStream
 import java.io.IOException
+import kotlin.jvm.Throws
 
 
 @Suppress("DEPRECATION") //for the method getImage
@@ -32,6 +40,7 @@ class ImageFragment : Fragment() {
 
     //val
     private val binding get() = _binding!!
+
 
 
 //    override fun onCreate(savedInstanceState: Bundle?) {
@@ -53,16 +62,17 @@ class ImageFragment : Fragment() {
 //
 //    }
 
-
     private fun userTriggerButtons() {
         binding.switchContextText.setOnClickListener{
             imageIsText = binding.switchContextText.isChecked
+
         }
         binding.floatingButton.setOnClickListener{
             pickImage() //custom fun
         }
 
     }
+
     private fun pickImage(){
         val openGalleryIntent =
             Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
@@ -75,48 +85,56 @@ class ImageFragment : Fragment() {
         if (requestCode == 666) {
             if(resultCode == Activity.RESULT_OK)
             {
-                val bitmap = getImage(data)
-                if (bitmap != null) {
-                    binding.imageView.setImageBitmap(bitmap)
-                    processImageTagging(bitmap)
+                val bitmapImage = getImage(data)
+
+//                if (bitmap != null) {
+//                    binding.imageView.setImageBitmap(bitmap)
+//                    processImageTagging(bitmap)
+//                }
+                bitmapImage.apply {
+                    if(!imageIsText){
+                        if (bitmapImage != null) {
+                                val imageForIV =
+                                    context?.let { checkIfImageNeededRotation(it,bitmapImage,bitmapImage) } //TODO FIX THIS LINE
+
+                            binding.chipFailed.isVisible = false
+                            binding.imageView.setImageBitmap(imageForIV)
+                            processImageTagging(bitmapImage)
+                        }
+                    }
+                    if (imageIsText){
+                        if (bitmapImage != null) {
+                            binding.imageView.setImageBitmap(bitmapImage)
+                            startTextRecognizing(bitmapImage)
+                        }
+                    }
                 }
 
-//                val bitmap = getImage(data)
-//                    bitmap.apply {
-//                        binding.imageView.setImageBitmap(this)
-//                        if(!imageIsText){
-//                            if (bitmap != null) {
-//
-//                            }
-//                        }
-//                        if(imageIsText){
-//                            if (bitmap != null) {
-//                               // startTextRecognizing(bitmap)
-//                            }
-//                        }
-//                    }
                 }
             }
 
-//
-//
-//
-//           }
-//        }
+
         super.onActivityResult(requestCode, resultCode, data)
     }
 
-
-
-
-
-
-
-
-
-
-
-
+    @SuppressLint("SetTextI18n")
+    private fun startTextRecognizing(bitmap: Bitmap) {
+        val recognizer = TextRecognition.getClient()
+        val image = InputImage.fromBitmap(bitmap,0)
+        val result = recognizer.process(image)
+        binding.chipGroup.removeAllViews()
+        result.addOnSuccessListener {
+                binding.chipFailed.isVisible = true
+                binding.chipFailed.text = "i am text"// }
+            println("I AM TEXT BOOS")
+            }.addOnFailureListener {
+                // Task failed with an exception
+                // ...
+                binding.chipFailed.isVisible = true
+                binding.chipFailed.chipText = "i am text Fail"
+                println("I AM TEXT  FAIL BOSS")
+            }
+    }
 
     @SuppressLint("SetTextI18n", "ResourceAsColor")
     private fun processImageTagging(bitmap: Bitmap){
@@ -134,7 +152,7 @@ class ImageFragment : Fragment() {
 
         try {
             val image = InputImage.fromBitmap(bitmap,0)
-            fun olakala() {
+            fun objDetector(firstFailed :Boolean) {
                 //OBJECT DETECTOR
                 objectDetector.process(image)
                     .addOnSuccessListener {
@@ -155,22 +173,16 @@ class ImageFragment : Fragment() {
                                 null,
                                 R.style.Widget_MaterialComponents_Chip_Choice
                             ).apply {
-                                text = it
-                            }
+                                text = it }
                         }.forEach {
-                                binding.chipGroup.addView(it)
-                            }
+                                binding.chipGroup.addView(it) }
                     }.addOnFailureListener {
                         // Task failed with an exception
-                        binding.chipFailed.text = "I may be an AI app but not God, I can't know everything, I'm sorry." }
+                        if(firstFailed){
+                            binding.chipFailed.isVisible = true
+                            binding.chipFailed.text = "I may be an AI app but not God, I can't know everything, I'm sorry." }
+                    }
             }
-
-
-
-
-//ΣΧΟΛΙΑ
-
-            //val image = InputImage.fromBitmap(bitmap,0)
            // LABELER
             labeler.process(image)
                 .addOnSuccessListener {
@@ -182,78 +194,20 @@ class ImageFragment : Fragment() {
                      }
                          println("The labeler : $textLabeler")
                      }
+
                     //OBJECT DETECTOR
-                    objectDetector.process(image)
-                        .addOnSuccessListener {
-                            // Task completed successfully
-                            for (detectedObject in it) {
-                                for (label in detectedObject.labels) {
-                                    val textDetected = label.text
-                                    if (!listWithLabels.contains(textDetected)) {
-                                        listWithLabels.add(textDetected)
-
-                                    }
-                                }
-                            }
-                        }
-
-                    binding.chipGroup.removeAllViews()
-                    listWithLabels.sortedByDescending { it.length }.map {
-                        Chip(
-                            context,
-                            null,
-                            R.style.Widget_MaterialComponents_Chip_Choice
-                        ).apply {
-                            text = it
-                        }
-                    }
-                        .forEach {
-                            binding.chipGroup.addView(it)
-                        }
-                    listWithLabels.forEach(System.out::println)
+                    objDetector(false)
 
                 }.addOnFailureListener {
                     // Task failed with an exception
-                    olakala()
-
+                    objDetector(true)
                 }
-            //OBJECT DETECTOR
-            objectDetector.process(image)
-                .addOnSuccessListener {
-                    // Task completed successfully
-                    for (detectedObject in it) {
-                        for (label in detectedObject.labels) {
-                            val textDetected = label.text
-                                if (!listWithLabels.contains(textDetected)) {
-                                    listWithLabels.add(textDetected)
-
-                                }
-                        }
-                    }
-                    binding.chipGroup.removeAllViews()
-                    listWithLabels.sortedByDescending { it.length }.map {
-                        Chip(
-                            context,
-                            null,
-                            R.style.Widget_MaterialComponents_Chip_Choice
-                        ).apply {
-                            text = it
-                        }
-                    }
-                        .forEach {
-                            binding.chipGroup.addView(it)
-                        }
-                    listWithLabels.forEach(System.out::println)
-                }.addOnFailureListener {
-                    // Task failed with an exception
-                    binding.chipFailed.text = "I may be an AI app but not God, I can't know everything, I'm sorry." }
 
         } catch (e: IOException) {
+            binding.chipFailed.isVisible = true
             binding.chipFailed.text = "Something went wrong,try again."
             e.printStackTrace()
         }
-
-
     }
 
     //get image from data
@@ -262,12 +216,44 @@ class ImageFragment : Fragment() {
         return MediaStore.Images.Media.getBitmap(context?.contentResolver,selectedImage)
     }
 
+    //Get Image Uri from bitmap
+    private fun  getImageUri(context: Context, bitmap: Bitmap): Uri? {
+        val bytes =  ByteArrayOutputStream()
+        bitmap.compress( Bitmap.CompressFormat.JPEG,100,bytes)
+        val path = MediaStore.Images.Media.insertImage(context.contentResolver, bitmap, "imageUri", null)
+        return parse(path)
+    }
+
+    //Check if the photo needs rotation
+   @Throws(IOException::class)
+    private fun checkIfImageNeededRotation(context: Context, newImage: Bitmap, selectedImage: Bitmap): Bitmap? {
+        val selectedImageForRotation =  getImageUri(context,selectedImage)
+        val input = selectedImageForRotation?.let { context.contentResolver.openInputStream(it) }
+        val ei = ExifInterface(input!!)
+        return when (ei.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL)) {
+            ExifInterface.ORIENTATION_ROTATE_90 -> rotateImage(newImage, 90)
+            ExifInterface.ORIENTATION_ROTATE_180 -> rotateImage(newImage, 180)
+            ExifInterface.ORIENTATION_ROTATE_270 -> rotateImage(newImage, 270)
+
+            else -> newImage
+        }
+    }
+
+    private fun rotateImage(newImage: Bitmap, degree: Int): Bitmap? {
+        val matrix = Matrix()
+        matrix.postRotate(degree.toFloat())
+        val rotatedNewImage = Bitmap.createBitmap(newImage, 0, 0, newImage.width, newImage.height, matrix, true)
+        newImage.recycle()
+        return rotatedNewImage
+    }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
 }
+
+
 
 
 
