73bd48e3a256e0f8c57b4904b75959cf21e1650b
Kyprianos98 <kyprianos.andreou98@gmail.com>
2021-08-27 15:33:06 +0300

    Bug Fixes

--- app/src/main/java/unipi/p17168/imagesician/QrcodeFragment.kt
+++ app/src/main/java/unipi/p17168/imagesician/QrcodeFragment.kt
@@ -89,6 +89,7 @@ class QrcodeFragment : androidx.fragment.app.Fragment() {
                         }
                     }
                 }
+
                 cameraProvider.unbindAll() // Unbind any bound use cases before rebinding
                 cameraProvider.bindToLifecycle(
                     this,
