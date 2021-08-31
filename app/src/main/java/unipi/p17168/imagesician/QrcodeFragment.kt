8db191bdf1c381004a15764f240da6866e9d155b
zartilas <zartilasvasilios@protonmail.com>
2021-08-31 15:46:08 +0300

    QR CODE

--- app/src/main/java/unipi/p17168/imagesician/QrcodeFragment.kt
+++ app/src/main/java/unipi/p17168/imagesician/QrcodeFragment.kt
@@ -4,14 +4,13 @@ import android.Manifest.permission.CAMERA
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
+import androidx.camera.core.Preview;
+import android.util.Size
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.NonNull
-import androidx.camera.core.CameraSelector
-import androidx.camera.core.ImageAnalysis
-import androidx.camera.core.ImageProxy
-import androidx.camera.core.Preview
+import androidx.camera.core.*
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.content.ContextCompat
 import com.google.android.material.snackbar.Snackbar
@@ -27,8 +26,23 @@ import java.util.concurrent.Executors
 import java.util.concurrent.atomic.AtomicBoolean
 import com.google.mlkit.vision.barcode.BarcodeScanning
 import kotlinx.android.synthetic.main.fragment_image.*
+import com.google.common.util.concurrent.ListenableFuture
+import java.util.concurrent.ExecutionException
+import java.util.concurrent.Executor
 
-class QrcodeFragment : androidx.fragment.app.Fragment() , ImageAnalysis.Analyzer{
+import androidx.camera.core.ImageAnalysis
+
+import androidx.camera.core.CameraSelector
+import unipi.p17168.imagesician.utils.QrRecognizing
+import android.R
+
+import androidx.core.content.res.ResourcesCompat
+
+
+
+
+
+class QrcodeFragment : androidx.fragment.app.Fragment(){
 
     //var
     private var _binding: FragmentQrcodeBinding? = null
@@ -37,10 +51,13 @@ class QrcodeFragment : androidx.fragment.app.Fragment() , ImageAnalysis.Analyzer
     private val binding get() = _binding!!
     private val contextQrCode get() = this@QrcodeFragment.requireContext()
 
-    private val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA  // Select back camera
-    private val cameraExecutor = Executors.newSingleThreadExecutor()
-    private val processingBarcode = AtomicBoolean(false)
-    private val cameraProviderFuture = ProcessCameraProvider.getInstance(contextQrCode)
+    //private val scannerFragmentContext: ScannerFragment? = null
+    var processingBarcode = AtomicBoolean(false)
+    private val qrRecognizing: QrRecognizing = QrRecognizing()
+    private var cameraProviderFuture: ListenableFuture<ProcessCameraProvider>? = null
+    private var analysisExecutor: Executor? = null
+    private var camera: Camera? = null
+    var toggleFlash = false
 
 
 
@@ -59,146 +76,66 @@ class QrcodeFragment : androidx.fragment.app.Fragment() , ImageAnalysis.Analyzer
 
      private fun userTriggerButtons() {
         binding.scanButton.setOnClickListener{
-            startUserCamera()
+           // startUserCamera()
+            useCamera()
         }
      }
 
     private fun init() {
         ToolBox().setupPermissions(contextQrCode, requireActivity(), CAMERA, 234)
         userTriggerButtons()
-        startUserCamera()
+        useCamera()
+        //startUserCamera()
     }
 
-    private fun startUserCamera(){
-        cameraProviderFuture.addListener({
-            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
-            val preview = Preview.Builder().build().also {
-                it.setSurfaceProvider(
-                    binding.cameraPreviewView.surfaceProvider
-                )
-            }
+
+    private fun useCamera(){
+        analysisExecutor = Executors.newSingleThreadExecutor()
+        cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
+        cameraProviderFuture!!.addListener(Runnable {
             try {
-                val imageAnalysis = ImageAnalysis.Builder()
-                    .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
-                    .build().also {
-                    it.setAnalyzer(cameraExecutor) { imageProxy: ImageProxy? ->
-                        if (processingBarcode.compareAndSet(false, true)) {
-                            if (imageProxy != null) {
-                                Log.wtf("MyApp 66", "I am here boss")
-
-                                /*QrRecognizing().*/analyze(imageProxy)
-                                    /*  val type =  QrRecognizing().typeOfQR*/
-
-                               /*     when (QrRecognizing().typeOfQR){
-                                        true-> {Log.wtf("1","I am true boss")}
-                                        false-> {Log.wtf("2","I am false boss")}
-                                        null-> {Log.wtf("3","I am null boss , sorry")}
-
-                                }*/
-                            }
-                        }
-                    }
-                }
-
-                cameraProvider.unbindAll() // Unbind any bound use cases before rebinding
-                cameraProvider.bindToLifecycle(
-                    this,
-                    cameraSelector,
-                    preview,
-                    imageAnalysis
-                )  // Bind use cases to lifecycleOwner
-
-            } catch (e: Exception) {
-                ToolBox().showSnackBar(
-                    this@QrcodeFragment.requireView(),
-                    ContextCompat.getColor(contextQrCode, R.color.colorErrorBackgroundSnackbar),
-                    ContextCompat.getColor(contextQrCode, R.color.colorStrings),
-                    "Something went wrong!",
-                    "OK",
-                    Snackbar.ANIMATION_MODE_SLIDE
-                ).show()
-                Log.e("PreviewUseCase", "Binding failed! :(", e)
+                val cameraProvider = cameraProviderFuture!!.get()
+                bindPreview(cameraProvider)
+            } catch (ignored: ExecutionException) {
+            } catch (ignored: InterruptedException) {
             }
-        }, ContextCompat.getMainExecutor(contextQrCode))
+        }, ContextCompat.getMainExecutor(requireContext()))
     }
 
 
-    private val options = BarcodeScannerOptions.Builder()
-        .setBarcodeFormats(
-            Barcode.FORMAT_QR_CODE,
-            Barcode.FORMAT_AZTEC)
-        .build()
+    private fun bindPreview(cameraProvider: ProcessCameraProvider) {
 
-    var listWithResult = arrayListOf<String>()
+        val preview = Preview.Builder()
+            .build()
 
-//    var typeOfQR: Boolean? = null
+        val cameraSelector = CameraSelector.Builder()
+            .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+            .build()
 
-    private val scanner = BarcodeScanning.getClient(options)
+        val imageAnalysis = ImageAnalysis.Builder()
+            .setTargetResolution(Size(1280, 720))
+            .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+            .build()
+        preview.setSurfaceProvider( binding.cameraPreviewView.surfaceProvider)
 
-    @SuppressLint("UnsafeOptInUsageError")
-    override fun analyze(imageProxy: ImageProxy) {
-
-        val mediaImage = imageProxy.image
-        if (mediaImage != null) {
-
-            val image = InputImage.fromMediaImage(mediaImage, imageProxy.imageInfo.rotationDegrees)
-            scanner.process(image).addOnSuccessListener { barcodes ->
-                // Task completed successfully
-                for (barcode in barcodes) {
-                    // barcodeListener( barcode.rawValue)
-                    val bounds = barcode.boundingBox
-                    val corners = barcode.cornerPoints
-                    val rawValue = barcode.rawValue
-                    Log.wtf("MyApp 69:","I AM HERE BOSS !!")
-                    Log.e("bounds:","$bounds")
-                    Log.e("corners:","$corners")
-                    Log.wtf("rawValue:",rawValue)
-                    // See API reference for complete list of supported types
-                    when (barcode.valueType) {
-                        Barcode.TYPE_WIFI -> {
-                            val ssid = barcode.wifi!!.ssid
-                            val password = barcode.wifi!!.password
-                            val type = barcode.wifi!!.encryptionType
-                            Log.wtf("test:","I AM WIFI BOSS")
-//                            typeOfQR = true
-                            listWithResult.add(ssid!!)
-                            listWithResult.add(password!!)
-                            listWithResult.add(type.toString())
-                        }
-                        Barcode.TYPE_URL -> {
-                            Log.wtf("test:","I AM URL BOSS")
-                            val title = barcode.url!!.title
-                            val url = barcode.url!!.url
-//                            typeOfQR = false
-                            listWithResult.add(title!!)
-                            listWithResult.add(url!!)
-
-                            Log.e("My URL IS HERE BOSS:",url)
-                        }
-                        Barcode.FORMAT_ALL_FORMATS->{
-//                            typeOfQR = null
-                            val value = barcode.displayValue
-                            listWithResult.add(value!!)
-                            Log.wtf("test:",value)
-
-                        }
+        analysisExecutor?.let {
+            imageAnalysis.setAnalyzer(it, { imageProxy: ImageProxy? ->
+                if (processingBarcode.compareAndSet(false, true)) {
+                    if (imageProxy != null) {
+                        Log.wtf("MyApp 66", "I am here boss")
+                        qrRecognizing.analyze(imageProxy)
                     }
                 }
-            }.addOnFailureListener {
-                // Task failed with an exception
-                ToolBox().showSnackBar(
-                    this@QrcodeFragment.requireView(),
-                    ContextCompat.getColor(contextQrCode, R.color.colorErrorBackgroundSnackbar),
-                    ContextCompat.getColor(contextQrCode, R.color.colorStrings),
-                    "Something went wrong!",
-                    "OK",
-                    Snackbar.ANIMATION_MODE_SLIDE
-                ).show()
-                Log.wtf("PreviewUseCase", "Binding failed! :( boss ")
-            }.addOnCompleteListener {
-                // It's important to close the imageProxy
-                imageProxy.close()
-            }
+            })
         }
+        cameraProvider.unbindAll()
+        camera = cameraProvider.bindToLifecycle(this, cameraSelector, imageAnalysis, preview)
     }
+
+    override fun onResume() {
+        super.onResume()
+        processingBarcode.set(false)
+    }
+
+
 }
--- app/src/main/java/unipi/p17168/imagesician/utils/QrRecognizing.kt
+++ app/src/main/java/unipi/p17168/imagesician/utils/QrRecognizing.kt
@@ -1,11 +1,12 @@
-/*
 package unipi.p17168.imagesician.utils
 
+
 import android.annotation.SuppressLint
+import android.media.Image
+import android.os.Bundle
 import android.util.Log
 import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.ImageProxy
-import androidx.core.content.ContentProviderCompat.requireContext
 import com.google.mlkit.vision.barcode.Barcode
 import com.google.mlkit.vision.barcode.BarcodeScannerOptions
 import com.google.mlkit.vision.barcode.BarcodeScanning
@@ -14,75 +15,54 @@ import com.google.mlkit.vision.common.InputImage
 
 class QrRecognizing : ImageAnalysis.Analyzer {
 
-
-    private val options = BarcodeScannerOptions.Builder()
-        .setBarcodeFormats(
-            Barcode.FORMAT_QR_CODE,
-            Barcode.FORMAT_AZTEC)
+    private var options = BarcodeScannerOptions.Builder()
+        .setBarcodeFormats(Barcode.FORMAT_QR_CODE)
         .build()
+    var barcodeScanner = BarcodeScanning.getClient(options)
 
     var listWithResult = arrayListOf<String>()
 
 //    var typeOfQR: Boolean? = null
 
-    private val scanner = BarcodeScanning.getClient(options)
-
-    @SuppressLint("UnsafeOptInUsageError")
     override fun analyze(imageProxy: ImageProxy) {
-
-        val mediaImage = imageProxy.image
+        @SuppressLint("UnsafeOptInUsageError")
+        val mediaImage: Image? = imageProxy.image
         if (mediaImage != null) {
+            val image = InputImage.fromMediaImage(mediaImage, imageProxy.imageInfo.rotationDegrees)
+            Log.wtf("MyApp 636", "I am here boss")
+            barcodeScanner.process(image).addOnSuccessListener {  barcodes ->
+                Log.wtf("MyApp 1312", "I am here boss")
 
-        val image = InputImage.fromMediaImage(mediaImage, imageProxy.imageInfo.rotationDegrees)
-        scanner.process(image).addOnSuccessListener { barcodes ->
-                // Task completed successfully
-                for (barcode in barcodes) {
-                    // barcodeListener( barcode.rawValue)
-                    val bounds = barcode.boundingBox
-                    val corners = barcode.cornerPoints
-                    val rawValue = barcode.rawValue
-                    Log.wtf("MyApp 69:","I AM HERE BOSS !!")
-                    Log.e("bounds:","$bounds")
-                    Log.e("corners:","$corners")
-                    Log.wtf("rawValue:",rawValue)
-                    // See API reference for complete list of supported types
-                    when (barcode.valueType) {
-                        Barcode.TYPE_WIFI -> {
-                            val ssid = barcode.wifi!!.ssid
-                            val password = barcode.wifi!!.password
-                            val type = barcode.wifi!!.encryptionType
-                            Log.wtf("test:","I AM WIFI BOSS")
-//                            typeOfQR = true
-                            listWithResult.add(ssid!!)
-                            listWithResult.add(password!!)
-                            listWithResult.add(type.toString())
-                        }
-                        Barcode.TYPE_URL -> {
-                            Log.wtf("test:","I AM URL BOSS")
-                            val title = barcode.url!!.title
-                            val url = barcode.url!!.url
-//                            typeOfQR = false
-                            listWithResult.add(title!!)
-                            listWithResult.add(url!!)
+                if (barcodes != null && barcodes.isNotEmpty()) {
 
-                            Log.e("My URL IS HERE BOSS:",url)
-                        }
-                        Barcode.FORMAT_ALL_FORMATS->{
-//                            typeOfQR = null
-                            val value = barcode.displayValue
-                            listWithResult.add(value!!)
-                            Log.wtf("test:",value)
+                    Log.wtf("MyApp 1000", "I am here boss")
+                    for (barcode in barcodes) {
+                        Log.wtf("MyApp 655", "I am here boss")
+                        Log.wtf("My code", barcode.rawValue)
 
+                        when (barcode.valueType) {
+                            Barcode.TYPE_WIFI -> {
+                                val ssid = barcode.wifi!!.ssid
+                                val password = barcode.wifi!!.password
+                                val type = barcode.wifi!!.encryptionType
+                                Log.wtf("test:", "I AM WIFI BOSS")
+                            }
+                            Barcode.TYPE_URL -> {
+                                Log.wtf("test:", "I AM URL BOSS")
+                                val title = barcode.url!!.title
+                                val url = barcode.url!!.url
+                                Log.e("My URL IS HERE BOSS:", url)
+                            }
                         }
                     }
                 }
-            }.addOnFailureListener {
-                // Task failed with an exception
-            }.addOnCompleteListener {
-                // It's important to close the imageProxy
-                imageProxy.close()
             }
+                .addOnFailureListener {}
+                .addOnCompleteListener {
+                    // It's important to close the imageProxy
+                    imageProxy.close()
+                }
         }
     }
+
 }
-*/
